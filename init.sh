4992e3773defbda65a7e8eaa4d26ea705ca3f3ee
Thomas Derksen <tde@team-mueller.net>
2024-12-04 13:12:22 +0100

    fixed theme name in init

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 wp core install \
-  --url=http://sage-starter.lndo.site/ \
+  --url=http://bedrock-sage-boilerplate.lndo.site/ \
   --title="Sage Starter" \
   --admin_user=admin \
   --admin_password=password \
-  --admin_email=admin@sage-starter.lndo.site \
+  --admin_email=admin@bedrock-sage-boilerplate.lndo.site \
   --path=web/wp
-wp theme activate sage
+wp theme activate sage-boilerplate
